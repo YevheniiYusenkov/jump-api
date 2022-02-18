2dbb29cebbf330c8fce652d25f4d87d269f45952
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-19 01:17:39 +0200

    Update Linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,23 +5,27 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:@typescript-eslint/recommended'],
   root: true,
   env: {
     node: true,
     jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'object-curly-spacing': ['error', 'always'],
-    'array-bracket-spacing': ['error', 'always'],
-    'prettier/prettier': 0,
-  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'single'],
+        "comma-dangle": ['error', 'always-multiline'],
+        'object-curly-spacing': ['error', 'always'],
+        'array-bracket-spacing': ['error', 'always'],
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+      }
+    }
+  ]
 };
