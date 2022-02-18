3e453b07c959ac09f991606cc74e68d5221f8e28
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-19 01:20:26 +0200

    Import dependencies in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 
+import { AbillsModule } from '@jump/abills';
+import { GatewayConfigModule } from '@jump/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { InternetModule } from '../internet/internet.module';
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GatewayConfigModule,
+    InternetModule,
+    AbillsModule,
+  ],
+  controllers: [ AppController ],
+  providers: [ AppService ],
 })
 export class AppModule {}
