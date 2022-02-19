f601093e2632bb9b0fcf2b76d4231003055fecff
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-19 20:23:52 +0200

    Pretty

--- libs/abills/interfaces/abills.options.async.interface.ts
+++ libs/abills/interfaces/abills.options.async.interface.ts
@@ -3,8 +3,7 @@ import { ModuleMetadata, Type } from '@nestjs/common';
 import { AbillsOptionsFactory } from './abills.options.factory.interface';
 import { AbillsOptions } from './abills.options.interface';
 
-export interface AbillsOptionsAsync
-  extends Pick<ModuleMetadata, 'imports'> {
+export interface AbillsOptionsAsync extends Pick<ModuleMetadata, 'imports'> {
   inject?: any[];
   useExisting?: Type<AbillsOptionsFactory>;
   useClass?: Type<AbillsOptionsFactory>;
