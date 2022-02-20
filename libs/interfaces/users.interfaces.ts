faec900a898d89297bb453897c36a8d365a62ec3
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-20 16:50:44 +0200

    Transform users.interface to validate classes

--- libs/interfaces/users.interfaces.ts
+++ libs/interfaces/users.interfaces.ts
@@ -1,20 +1,51 @@
-export interface CreateUserPayload {
-  fullName: string,
-  phoneNumber: string,
-  plan: number,
-  locality: string,
-  street: string,
-  suite: string,
-  apartment?: string,
-  username: string,
-  password: string,
-  confirmPassword: string,
+import { IsNumber, IsOptional, IsPhoneNumber, IsString } from 'class-validator';
+
+import { Match } from '@jump/decorators';
+
+export class CreateUserDTO {
+  @IsString()
+  fullName: string;
+  
+  @IsString()
+  @IsPhoneNumber('UA')
+  phoneNumber: string;
+  
+  @IsNumber()
+  plan: number;
+  
+  @IsString()
+  locality: string;
+  
+  @IsString()
+  street: string;
+  
+  @IsString()
+  suite: string;
+  
+  @IsString()
+  @IsOptional()
+  apartment?: string;
+  
+  @IsString()
+  username: string;
+  
+  @IsString()
+  password: string;
+  
+  @IsString()
+  @Match('password')
+  confirmPassword: string;
 }
 
-export interface CreateAbillsUserPayload {
-  login: string,
-  password: string,
-  createBill?: number,
+export class CreateAbillsUserPayload {
+  @IsString()
+  login: string;
+  
+  @IsString()
+  password: string;
+  
+  @IsString()
+  createBill?: number;
 }
 
 export interface AbillsUser {
