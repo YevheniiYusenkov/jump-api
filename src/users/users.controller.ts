d7b9cb900cd9fbdd22362fcaa01e181b01e7aac2
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-20 15:46:53 +0200

    Create createUser in UsersModule

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+
+import { AbillsUser, CreateUserPayload } from '@jump/interfaces';
+
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly service: UsersService) {}
+  
+  @Post()
+  public async createUser(payload: CreateUserPayload): Promise<AbillsUser>  {
+    return await this.service.createUser(payload);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
+import { AbillsService } from '@jump/abills';
+import { AbillsUser, CreateUserPayload } from '@jump/interfaces';
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  public constructor(private readonly abillsService: AbillsService) {}
+  
+  public async createUser(payload: CreateUserPayload): Promise<AbillsUser> {
+    return await this.abillsService.createUser({
+      login: payload.username,
+      password: payload.password,
+    });
+  }
+}
