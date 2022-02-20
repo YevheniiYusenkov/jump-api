2123f41a9d2e6840b1478cd602c66013444af9d7
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-20 16:50:02 +0200

    Create Match decorator

---
+++ libs/decorators/match.decorator.ts
@@ -0,0 +1,24 @@
+import { registerDecorator, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
+
+export function Match(property: string, validationOptions?: ValidationOptions): any {
+  // @typescript-eslint/explicit-module-boundary-types
+  return (object: any, propertyName: string) => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [ property ],
+      validator: MatchConstraint,
+    });
+  };
+}
+
+@ValidatorConstraint({ name: 'Match' })
+export class MatchConstraint implements ValidatorConstraintInterface {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  public validate(value: any, args: ValidationArguments): boolean {
+    const [ relatedPropertyName ] = args.constraints;
+    const relatedValue = (args.object as any)[relatedPropertyName];
+    return value === relatedValue;
+  }
+}
