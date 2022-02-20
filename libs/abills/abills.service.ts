6140e942918fa02e0d1f2ffc6cea8721221a6260
Yevhenii Yusenkov <yevhenii.yusenkov@gmail.com>
2022-02-20 14:29:07 +0200

    Create createUser request

--- libs/abills/abills.service.ts
+++ libs/abills/abills.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import axios from 'axios';
 
-import { GetAbillsPlansResponse, GetPlansResponse, Plan } from '@jump/interfaces';
+import { AbillsPlan, Plan, CreateUserPayload, AbillsUser } from '@jump/interfaces';
 
 import { AbillsOptions } from './interfaces/abills.options.interface';
 
@@ -9,8 +9,8 @@ import { AbillsOptions } from './interfaces/abills.options.interface';
 export class AbillsService {
   public constructor(@Inject('ABILLS_CONFIG') private config: AbillsOptions) {}
   
-  private async get<T>(location: string): Promise<T> {
-    const response = await axios.get<T>(this.config.url + location, {
+  private async get<ResponseType>(location: string): Promise<ResponseType> {
+    const response = await axios.get<ResponseType>(this.config.url + location, {
       headers: {
         KEY: this.config.key,
       },
@@ -19,11 +19,26 @@ export class AbillsService {
     return response.data;
   }
   
-  public async plans(): Promise<GetPlansResponse> {
-    return (await this.get<GetAbillsPlansResponse>('/abon/tariffs')).map((plan): Plan => ({
+  private async post<ResponseType, PayloadType>(location: string, body?: PayloadType): Promise<ResponseType> {
+    const response = await axios.post<ResponseType>(this.config.url + location, {
+      headers: {
+        KEY: this.config.key,
+      },
+      data: body,
+    });
+    
+    return response.data;
+  }
+  
+  public async plans(): Promise<Plan[]> {
+    return (await this.get<AbillsPlan[]>('/abon/tariffs')).map((plan): Plan => ({
       id: plan.tpId,
       name: plan.name,
       description: plan.description,
     }));
+  }
+  
+  public async createUser(payload: CreateUserPayload): Promise<AbillsUser> {
+    return await this.post<AbillsUser, CreateUserPayload>('/users', payload);
   }
 }
